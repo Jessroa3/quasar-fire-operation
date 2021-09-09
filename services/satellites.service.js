2ca4cc0b603da89fd5889105120fdf203e5c0c64
Jessroa3 <jessicaroa3@gmail.com>
2021-09-08 21:52:43 -0300

    view and services

---
+++ services/satellites.service.js
@@ -0,0 +1,31 @@
+'use strict';
+const satellites = require('../config/coordinates_satellites.json');
+const fs = require('fs');
+var path = require('path');
+
+
+const get = function(name){
+    return satellites[name]
+}
+
+const getAll = function(){
+    return satellites;
+}
+
+const setDataSatellite = function(satellite){
+  satellites[satellite.name].distance = satellite.distance
+  satellites[satellite.name].message = satellite.message
+  var json = JSON.stringify(satellites, null, 2);
+  var jsonPath = path.join(__dirname,'../config/coordinates_satellites.json');
+  fs.writeFile(jsonPath, json, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  })
+}
+
+
+module.exports = {
+    get,
+    getAll,
+    setDataSatellite
+};
